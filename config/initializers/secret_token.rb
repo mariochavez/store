2175c0b865ed0fa0d44a4f51de962b62c742774c
Mario A Chavez <mario.chavez@gmail.com>
2013-02-01 08:49:22 -0600

    With todays Rails update secret_token key has been changed to to secret_key_base for encrypted cookies

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Store::Application.config.secret_token = 'f5cd8a92326f3b7febb11be434ac7a94daaf7c1c63f534451757b734ca3b77a9e7496b99f525b5b8d2a2ccc11917246ebc8f610f5a30f857a09c9bdc993eb84f'
+Store::Application.config.secret_key_base = 'f5cd8a92326f3b7febb11be434ac7a94daaf7c1c63f534451757b734ca3b77a9e7496b99f525b5b8d2a2ccc11917246ebc8f610f5a30f857a09c9bdc993eb84f'
